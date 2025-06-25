b67ae901a3933b43edeb861d4e3821158d50b948
CP <catp757@proton.me>
2025-06-25 18:25:07 -0400

    Fix Python linting issues with autopep8

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://catp757-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = [
+    'localhost',
+    'https://catp757-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
     'https://catp757-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
     'https://catp757-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
@@ -146,4 +148,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
