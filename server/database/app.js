e2e01706c87cc735c331ec9da8571aff050e811f
catp757 <158765774+catp757@users.noreply.github.com>
2025-06-25 20:41:36 -0400

    More changes after linting javascript code

--- server/database/app.js
+++ server/database/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -20,10 +20,10 @@ const Dealerships = require('./dealership');
 
 try {
   Reviews.deleteMany({}).then(()=>{
-    Reviews.insertMany(reviews_data['reviews']);
+    Reviews.insertMany(reviews_data.reviews);
   });
   Dealerships.deleteMany({}).then(()=>{
-    Dealerships.insertMany(dealerships_data['dealerships']);
+    Dealerships.insertMany(dealerships_data.dealerships);
   });
   
 } catch (error) {
@@ -33,7 +33,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -93,7 +93,6 @@ app.get('/fetchDealers', async (req, res) => {
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   let data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } );
 
   const review = new Reviews({
     name: data.name,
